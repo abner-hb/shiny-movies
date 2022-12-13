4468351abde0271ae1e9f60641255cfd161d31e1
Abner <113459290+abner-hb@users.noreply.github.com>
2022-12-13 13:16:52 -0500

    build_plot now uses get_movie_rows_sql to extract data

--- R/build_plot.R
+++ R/build_plot.R
@@ -1,10 +1,11 @@
 # source("R/get_movie_rows.R")
 # source("R/movie_plot.R")
-build_plot = function(xcol, ycol, cols_to_filter, containing){
-    fltrd_movies = get_movie_rows(
-        cols_to_filter = cols_to_filter, 
-        containing = containing
-    )
+build_plot = function(xcol, ycol, input_mat){
+    # fltrd_movies = get_movie_rows(
+    #     cols_to_filter = cols_to_filter, 
+    #     containing = containing
+    # )
+    fltrd_movies = get_movie_rows_sql(input_mat)
     plot = movie_plot(inputv = fltrd_movies, xcol = xcol, ycol = ycol)
     return(plot)
 }
