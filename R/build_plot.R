02ae8e4c431e38c1eb1f49ede9c9d16b5bc97327
jerrywangrs <runshuw@umich.edu>
2022-12-17 16:44:52 -0500

    Update build_plot.R

--- R/build_plot.R
+++ R/build_plot.R
@@ -1,5 +1,5 @@
-# source("R/get_movie_rows.R")
-# source("R/movie_plot.R")
+source("R/get_movie_rows.R")
+source("R/movie_plot.R")
 build_plot = function(xcol, ycol, input_mat){
     # fltrd_movies = get_movie_rows(
     #     cols_to_filter = cols_to_filter, 
