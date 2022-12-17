13208139a4d98d7c31541a483242fa0420bbfba5
jerrywangrs <runshuw@umich.edu>
2022-12-17 16:44:15 -0500

    Update movie_plot.R

--- R/movie_plot.R
+++ R/movie_plot.R
@@ -1,4 +1,9 @@
 #function plot. Function by Sihle
-movie_plot<-function(inputv, xcol, ycol){
-    ggplot(data=inputv, aes(x=.data[[xcol]], y=.data[[ycol]])) + geom_point()
+movie_plot<-function(inputv,xcol,ycol){
+    ggplot(data=inputv, aes(x=.data[[xcol]], y=.data[[ycol]], colour=title)) + 
+        geom_point(aes(size=.data[[ycol]]))+
+        theme(axis.text.x = element_text(angle=90, vjust = 0.5, hjust = 1))+
+        geom_text(aes(label=title), check_overlap = TRUE) +
+        theme(legend.position="none")
+    
 }
